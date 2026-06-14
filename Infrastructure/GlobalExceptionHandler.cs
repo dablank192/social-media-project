80dab70cc4d760bdd83328737315955bbb640af3
Quang Nguyen <quang200312@gmail.com>
2026-06-14 19:32:48 +0700

    Add: Implement global exception handler

---
+++ Infrastructure/GlobalExceptionHandler.cs
@@ -0,0 +1,61 @@
+using System;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using vsa_w_controller_csharp.Exception.AuthException;
+
+namespace vsa_w_controller_csharp.Infrastructure;
+
+public class GlobalExceptionHandler(
+    ILogger<GlobalExceptionHandler> logger,
+    IHostEnvironment env
+) : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext context,
+        System.Exception exception,
+        CancellationToken ct
+    )
+    {
+        logger.LogError(exception, exception.Message);
+
+        var problemDetail = new ProblemDetails
+        {
+            Title = "Error",
+            Detail= "An unknown error has occur",
+            Status= StatusCodes.Status500InternalServerError,
+            Instance= context.Request.Path
+        };
+
+        if(exception is DuplicateUsernameException)
+        {
+            context.Response.StatusCode = StatusCodes.Status200OK;
+            problemDetail.Detail = "User is already exist";
+            problemDetail.Status = StatusCodes.Status409Conflict;
+        }
+
+        else if (exception is InvalidCredentialsException)
+        {
+            context.Response.StatusCode = StatusCodes.Status200OK;
+            problemDetail.Detail = "Invalid username or password";
+            problemDetail.Status = StatusCodes.Status401Unauthorized;
+        }
+
+        else if (exception is InvalidRefreshTokenException)
+        {
+            context.Response.StatusCode = StatusCodes.Status200OK;
+            problemDetail.Detail = "Can't identify refresh token";
+            problemDetail.Status = StatusCodes.Status401Unauthorized;
+        }
+
+        if (env.IsDevelopment())
+        {
+            problemDetail.Extensions.Add("Detail", exception.Message);
+            problemDetail.Extensions.Add("Traceback", exception.StackTrace);
+        }
+
+        await context.Response.WriteAsJsonAsync(problemDetail, ct);
+
+        return true;
+    }
+}
