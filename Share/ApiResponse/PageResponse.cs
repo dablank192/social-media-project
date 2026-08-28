33a00fa0b03f9c0866401930cd60fbd8e2e125aa
Quang Nguyen <quang200312@gmail.com>
2026-08-28 23:52:41 +0700

    Feat: Finished API response template

---
+++ Share/ApiResponse/PageResponse.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace vsa_w_controller_csharp.Share.ApiResponse;
+
+public record PageResponse<T>(
+    List<T> Data,
+    int PageIndex,
+    int PageRecord,
+    int TotalRecord,
+    int TotalPage
+); //response template cho API có sử dụng phân trang
+// Khi sử dụng template phân trang này, lấy dữ liệu kết quả của API Handle, sau đó gán dữ liệu đó vào type của template này, và gán template này vào repsonse template cha trước khi trả về cho Client thông qua return Ok()
+// Chưa test
